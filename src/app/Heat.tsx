38d868f6a2eccacc273621c0619b69804b243f4b
Hamza Javed <hamzakhan.hk1994@gmail.com>
2025-02-06 01:32:42 +0500

    head

---
+++ src/app/Heat.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+    return (
+      <>
+        <meta name="google-site-verification" content="jTJYQSJIQYqg3Zl39OJyS6LC5mcWAOn1kZvlSJmPz3E" />
+      </>
+    );
+  }
+  
